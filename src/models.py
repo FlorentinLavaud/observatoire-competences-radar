61a50b775c0cde1b4dd3efd378dcca2e16d596d4
Florentin <116577812+FlorentinLavaud@users.noreply.github.com>
2026-06-09 23:40:28 +0200

    solde import erro on pydantic

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 # src/models.py
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field
 from typing import Optional, List, Dict, Any
 from datetime import datetime
 
